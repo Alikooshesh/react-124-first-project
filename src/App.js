9e5a0ab1e711233f0e38c3fd80e909c390bff85e
Ali Kooshesh <alikooshesh007@gmail.com>
2025-01-24 11:34:08 +0330

    feat(app): add event listener

--- src/App.js
+++ src/App.js
@@ -4,32 +4,16 @@ import Name from './components/name/name'
 
 function App() {
 
-  const flag = true
-
-  // if(!flag){
-  //   return <p>you cant see this component</p>
-  // }
+  function handleBtnClick(id){
+    alert(id)
+  }
 
   return (
-    <div className="App">
-      {flag && <Name name="Reza" age={27} hasError={true}/>}
-      {flag ? <Name name="Reza" age={27}/> : <p>you cant see this component</p>}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Name name="ali" age="25"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={()=>{
+      handleBtnClick("test")
+    }}>
+      click me!
+    </button>
   );
 }
 
