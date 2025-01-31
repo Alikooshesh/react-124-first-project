b4f8fbba691fee93f301c300e5e04d94c5713781
Ali Kooshesh <alikooshesh007@gmail.com>
2025-01-31 11:07:31 +0330

    feat(app): simple form validation with regex

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import {useEffect, useState} from 'react'
 import './App.css';
 
-
+const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 function App() {
-  const [data,setData] = useState([])
-  
-  async function deleteTodo(id) {
-    try{
-      const response = await fetch(`https://66d1843962816af9a4f3ec69.mockapi.io/todo/${id}`,{
-        method : "DELETE"
-      })
-      if(response.ok){
-        let temp = data
-        temp = temp.filter(item => item.id !== id)
-        setData(temp)
-      }
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
-
-  async function getTodoList() {
-    try{
-      const response = await fetch("https://66d1843962816af9a4f3ec69.mockapi.io/todo");
-      const result = await response.json()
-      setData(result)
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
+  const [email,setEmail] = useState("")
+  const [err,setErr] = useState(true)
 
   useEffect(()=>{
-    getTodoList()
-  },[])
+    if(EMAIL_REGEX.test(email)){
+      setErr(false)
+    }else{
+      setErr(true)
+    }
+  },[email])
 
   return (
     <div>
-      {data.map(d => (
-        <>
-          <p>{d.title}</p>
-          <button onClick={()=> deleteTodo(d.id)}>delete</button>
-        </>
-      ))}
+      <input value={email} onChange={(e)=> setEmail(e.target.value)}/>
+      <button disabled={err} onClick={()=> alert("it submitted")}>submit</button>
     </div>
   );
 }
