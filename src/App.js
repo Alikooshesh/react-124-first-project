15eb20cdcf3239ef2e8bc02f8d6b626e15819c40
Ali Kooshesh <alikooshesh007@gmail.com>
2025-01-31 11:27:02 +0330

    feat(app): add some icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {useEffect, useState} from 'react'
 import './App.css';
+import { FaBeer } from "react-icons/fa";
+import { BsAirplaneEnginesFill } from "react-icons/bs";
 
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
@@ -17,7 +19,13 @@ function App() {
 
   return (
     <div>
+      <FaBeer color='green'/>
+      
+
+      <div>
       <input value={email} onChange={(e)=> setEmail(e.target.value)}/>
+      <BsAirplaneEnginesFill/>
+      </div>
       <button disabled={err} onClick={()=> alert("it submitted")}>submit</button>
     </div>
   );
