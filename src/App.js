665a97a360d548328772d5409c26bd6cc60488a6
Ali Kooshesh <alikooshesh007@gmail.com>
2025-01-31 09:25:45 +0330

    feat(app): add simple useeffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css';
-import Decrease from './components/decrease';
+
 
 
 function App() {
 
   const [name , setName] = useState("test")
 
+  useEffect(()=>{
+    console.log("whath me!")
+  },[name])
+
+  useEffect(()=>{
+    console.log("it will run just one time!")
+  },[])
+
   return (
     <div>
       <label>name :</label>
       <input value={name} onChange={(e)=> {
-        if(e.target.value.length<5){
-          setName(e.target.value)
-        }
+        setName(e.target.value)
       }}/>
       {name.length === 4 && <small>cant type more than 4 letters</small>}
 
