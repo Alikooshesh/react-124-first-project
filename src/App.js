329ea8e19813b78f03a316ba15cf5610f6ff5a59
Ali Kooshesh <alikooshesh007@gmail.com>
2025-01-31 10:17:02 +0330

    simple todo with api

--- src/App.js
+++ src/App.js
@@ -4,28 +4,47 @@ import './App.css';
 
 
 function App() {
+  const [data,setData] = useState([])
+  
+  async function deleteTodo(id) {
+    try{
+      const response = await fetch(`https://66d1843962816af9a4f3ec69.mockapi.io/todo/${id}`,{
+        method : "DELETE"
+      })
+      if(response.ok){
+        let temp = data
+        temp = temp.filter(item => item.id !== id)
+        setData(temp)
+      }
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
 
-  const [name , setName] = useState("test")
+  async function getTodoList() {
+    try{
+      const response = await fetch("https://66d1843962816af9a4f3ec69.mockapi.io/todo");
+      const result = await response.json()
+      setData(result)
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
 
   useEffect(()=>{
-    console.log("whath me!")
-  },[name])
-
-  useEffect(()=>{
-    console.log("it will run just one time!")
+    getTodoList()
   },[])
 
   return (
     <div>
-      <label>name :</label>
-      <input value={name} onChange={(e)=> {
-        setName(e.target.value)
-      }}/>
-      {name.length === 4 && <small>cant type more than 4 letters</small>}
-
-      <p>name is {name}</p>
-      
-      <input value="test"/>
+      {data.map(d => (
+        <>
+          <p>{d.title}</p>
+          <button onClick={()=> deleteTodo(d.id)}>delete</button>
+        </>
+      ))}
     </div>
   );
 }
